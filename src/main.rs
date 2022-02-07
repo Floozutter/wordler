deac22605c665b4dbfdb7f18414dc2d94dc837e5
Floozutter <me@floozutter.site>
2022-02-07 13:33:51 -0800

    set settings for subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
-    let app = clap::app_from_crate!();
+    let app = clap::app_from_crate!()
+        .global_setting(clap::AppSettings::PropagateVersion)
+        .global_setting(clap::AppSettings::UseLongFormatForHelpSubcommand)
+        .setting(clap::AppSettings::SubcommandRequiredElseHelp);
     let matches = app.get_matches();
     println!("{matches:?}");
 }
