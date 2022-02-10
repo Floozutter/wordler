76f8fbdac3b6c9ec29f0769d45e89ad2f2f52718
Floozutter <me@floozutter.site>
2022-02-10 00:25:18 -0800

    order subcommands alphabetically

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ fn main() {
         .global_setting(clap::AppSettings::UseLongFormatForHelpSubcommand)
         .setting(clap::AppSettings::SubcommandRequiredElseHelp)
         .subcommand(
-            clap::App::new("solve")
-                .about("play a game of Wordle")
-        )
-        .subcommand(
             clap::App::new("blind")
                 .about("predetermine guesses without seeing any hints")
+        )
+        .subcommand(
+            clap::App::new("solve")
+                .about("play a game of Wordle")
         );
     let matches = app.get_matches();
     println!("{matches:?}");
