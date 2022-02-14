906135ac9e57bbf1ef6ebe8e945869f694e811e0
Floozutter <me@floozutter.site>
2022-02-13 21:50:38 -0800

    remove help subcommand
    reason 1: couldn't figure out how to lowercase the about text
    reason 2: running `cargo run -- help help` crashed the program lol

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     let app = clap::app_from_crate!()
         .global_setting(clap::AppSettings::PropagateVersion)
-        .global_setting(clap::AppSettings::UseLongFormatForHelpSubcommand)
         .setting(clap::AppSettings::SubcommandRequiredElseHelp)
+        .setting(clap::AppSettings::DisableHelpSubcommand)
         .mut_arg("help", |a| a.help("print help information"))
         .mut_arg("version", |a| a.help("print version information").short('v'))
         .subcommand(
